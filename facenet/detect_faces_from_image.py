924f1e7d26fdd6671741d26b01c8021eea119dab
enningxie <enningxie@163.com>
2018-06-07 19:59:10 +0800

    x

--- facenet/detect_faces_from_image.py
+++ facenet/detect_faces_from_image.py
@@ -1,6 +1,8 @@
 import argparse
 import face_recognition as fr
 import cv2
+import os
+from utils import image_resize, convert_bgr_to_rgb
 
 
 FLAGS = None
@@ -8,12 +10,13 @@ FLAGS = None
 
 def parser():
     args = argparse.ArgumentParser()
-    args.add_argument('--image_path', default='/home/enningxie/Documents/DataSets/face_images/4200.jpg')
-    args.add_argument('--save_path', default='/home/enningxie/Documents/DataSets/face_rec/detec_images/4200.jpg')
+    args.add_argument('--image_path', default='/var/Data/xz/face/face_images')
+    args.add_argument('--save_path', default='/home/enningxie/Documents/DataSets/face_rec/detec_images/4200_02.jpg')
     return args.parse_args()
 
 
 def find_faces_in_pic(image_path, save_path):
+    cv2.imread(image_path)
     image = fr.load_image_file(image_path)
     face_locations = fr.face_locations(image, number_of_times_to_upsample=0, model="cnn")
     for top, right, bottom, left in face_locations:
@@ -22,15 +25,35 @@ def find_faces_in_pic(image_path, save_path):
 
         # Draw a label with a name below the face
         cv2.rectangle(image, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
-        rgb_frame = image[:, :, ::-1]
-    cv2.imwrite(save_path, rgb_frame)
+        # rgb_frame = image[:, :, ::-1]
+    cv2.imwrite(save_path, image)
     print('detect op end.')
+
+
+def count_in_pic(img_path):
+    images_list = os.listdir(img_path)
+    face_locations_list = []
+    face_locations_count = []
+    for image in images_list:
+        image_path = os.path.join(img_path, image)
+        image_data = cv2.imread(image_path)
+        image_resized = image_resize(image_data, (6144, 3240))
+        image_rgb = convert_bgr_to_rgb(image_resized)
+        face_locations = fr.face_locations(image_rgb, number_of_times_to_upsample=0, model="cnn")
+        face_locations_list.append(face_locations)
+        face_locations_count.append(len(face_locations))
+    return images_list, face_locations_count, face_locations_list
 
 
 if __name__ == '__main__':
     FLAGS = parser()
     image_path = FLAGS.image_path
     save_path = FLAGS.save_path
-    find_faces_in_pic(image_path, save_path)
+    images_list, face_locations_count, face_locations_list = count_in_pic(image_path)
+    max_num = max(face_locations_count)
+    for i, count in zip(images_list, face_locations_count):
+        if count == max_num:
+            print('{0} has {1} faces.'.format(i, count))
+
 
 
--- utils.py
+++ utils.py
@@ -5,6 +5,21 @@ import os
 FLAGS = None
 
 
+def image_resize(image, size):
+    img = cv2.imread(image)
+    img_resized = cv2.resize(img, size, interpolation=cv2.INTER_CUBIC)
+    print('resize op done.')
+    return img_resized
+
+
+
+# OpenCV follows BGR order, while matplotlib likely follows RGB order.
+def convert_bgr_to_rgb(bgr_img):
+    b, g, r = cv2.split(bgr_img)  # get b, g, r
+    rgb_img = cv2.merge([r, g, b])  # switch it to rgb
+    return rgb_img
+
+
 def convert_video_to_images(video_path, images_path, frame_number_per=100):
     input_movie = cv2.VideoCapture(video_path)
     frame_number = 0
@@ -30,7 +45,8 @@ def parser():
     args = argparse.ArgumentParser()
 
     args.add_argument('--video_path', default='/home/enningxie/Documents/DataSets/class_video/face/ch02_20180308161036.mp4')
-    args.add_argument('--images_path', default='/home/enningxie/Documents/DataSets/face_images')
+    args.add_argument('--images_path', default='/home/enningxie/Documents/DataSets/face_images/4200.jpg')
+    args.add_argument('--to_path', default='/home/enningxie/Documents/DataSets/face_rec/resize_images/4200.jpg')
     return args.parse_args()
 
 
@@ -38,4 +54,8 @@ if __name__ == '__main__':
     FLAGS = parser()
     video_path = FLAGS.video_path
     images_path = FLAGS.images_path
-    convert_video_to_images(video_path, images_path)
+    to_path = FLAGS.to_path
+    # convert_video_to_images(video_path, images_path)
+    # image_resize(images_path, (6144, 3240), to_path)
+    input_movie = cv2.VideoCapture(video_path)
+    print(input_movie.get(cv2.CAP_PROP_FPS))
